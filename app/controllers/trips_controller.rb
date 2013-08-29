5fef5cce9ccd3d79ed0df8da9fc7c93aa23ced9d
Tludi <tludi00@gmail.com>
2013-08-28 21:02:20 -0700

    changed json output to trips instead of waypoints

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,9 +1,9 @@
 class TripsController < ApplicationController
-
+  # http://shrouded-waters-9847.herokuapp.com/trips.json
   def index
     @trips = Trip.all
     @trip = Trip.first
     @waypoints = @trip.waypoints
-    render json: @waypoints
+    render json: @trips
   end
 end
